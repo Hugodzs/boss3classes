1059514f5a0b42fe5372c1a0af18002b79250bee
Hugo Silva <145476600+Hugodzs@users.noreply.github.com>
2023-10-25 12:41:43 +0000

    awes

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 class classes{
-    constructor(nome,tipo,ataque){
+    constructor(nome,tipo){
         this.nome = nome
         this.tipo = tipo
         this.ataque = ataque
     }
     attack(){
-        if(tipo = mago){this.ataque = "usou magia"}
-        else if(tipo = guerreiro){this.ataque = "usou espada" }
-        else if(tipo = monge){this.ataque = "usou artes marciais"}
-        else if(tipo = ninja){this.ataque = "usou shuriken"}
+        if(tipo === mago){this.ataque = "usou magia"}
+        else if(tipo === guerreiro){this.ataque = "usou espada" }
+        else if(tipo === monge){this.ataque = "usou artes marciais"}
+        else if(tipo === ninja){this.ataque = "usou shuriken"}
         else("erro")
     }
 }
+    let tipoMago = new classes("Hugo","mago")
+    console.log(tipoMago)
