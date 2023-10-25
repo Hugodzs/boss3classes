9fc8251ed8e185441690f7fdd5b67ca9d9ea2dbb
Hugo Silva <145476600+Hugodzs@users.noreply.github.com>
2023-10-25 14:16:09 +0000

    teste

--- index.js
+++ index.js
@@ -1,15 +1,36 @@
 class classes{
-    constructor(nome,tipo){
-        this.nome = nome
-        this.tipo = tipo
-    }
+    constructor(name, age, type){
+        this.name = name
+        this.age = age
+        this.type = type
+     }
     attack(){
-        if(tipo === mago){this.ataque = "usou magia"}
-        else if(tipo === guerreiro){this.ataque = "usou espada" }
-        else if(tipo === monge){this.ataque = "usou artes marciais"}
-        else if(tipo === ninja){this.ataque = "usou shuriken"}
-        else("erro")
+        let ataqueType = ""
+
+        switch(this.type){
+            case "guerreiro":
+                ataqueType = "espadas"
+                break
+            case "mago":
+                ataqueType = "bola de fogo"
+                break
+            case "cavaleiro":
+                ataqueType = "espada sagrada"
+                break
+            case "anão":
+                ataqueType = "machado de guerra"
+                break 
+            default:
+                ataqueType = "apenas suas mãos"
+                break  
+        }
+        console.log (`${this.name} o ${this.hero} atacou usando ${ataqueType}`)
     }
 }
-    let tipoMago = new classes("Hugo","mago")
-    console.log(tipoMago)
+
+    let tipoMago = new classes("Hugo","54","mago")
+    let tipoGuerreiro = new classes("Hugod","35","guerreiro")
+    let tipoMonge = new classes("higor","9","monge")
+    let tipoNinja = new classes("Higor","10","ninja")
+
+    
