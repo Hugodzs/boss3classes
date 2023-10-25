0a5bfd9dc40e05a8edf2a6464651f05cd5cead4e
Hugo Silva <145476600+Hugodzs@users.noreply.github.com>
2023-10-25 15:04:36 +0000

    tes

--- index.js
+++ index.js
@@ -24,13 +24,13 @@ class classes {
                 ataqueType = "apenas suas mãos"
                 break  
         }
-        console.log (`${this.name} de idade ${this.age} e da classe ${this.type} atacou usando ${ataqueType}`)
+        console.log (`${this.name} tem ${this.age} anos de idade e é da classe ${this.type} atacou usando ${ataqueType}`)
     }
 }
 
     let tipoMago = new classes("Hugo","54","mago")
     let tipoGuerreiro = new classes("Hugod","35","guerreiro")
-    let tipoMonge = new classes("higor","9","monge")
+    let tipoMonge = new classes("Higor","9","monge")
     let tipoNinja = new classes("Higor","10","ninja")
 
     tipoMonge.attack()
